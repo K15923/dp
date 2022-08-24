dcda6289f11b49191f9df3c38a5f5fc97caf5a82
wanglinhai <wanglinhai@ejiayou.com>
2022-08-24 17:57:38 +0800

    委托适配器

---
+++ src/com/dp/structural/adapter/entrust/Banner.java
@@ -0,0 +1,23 @@
+package com.dp.structural.adapter.entrust;
+
+/**
+ * @author k 2022/8/24 16:20
+ */
+public class Banner {
+
+    private String a;
+
+    public Banner(String a) {
+        this.a = a;
+    }
+
+    public void showWithParen(){
+        System.out.println("showWithParen_" + a + "");
+    }
+
+    public void showWithAster() {
+        System.out.println("showWithAster_" + a + "");
+    }
+
+
+}
---
+++ src/com/dp/structural/adapter/entrust/Main.java
@@ -0,0 +1,11 @@
+package com.dp.structural.adapter.entrust;
+
+/**
+ * @author k 2022/8/24 16:33
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
