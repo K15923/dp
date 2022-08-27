2118ed64a002944ed3db44e5260c4a2144a840f8
wanglinhai <wanglinhai@ejiayou.com>
2022-08-27 23:58:13 +0800

    装饰器模式

--- src/com/dp/structural/decorator/DecoratorMain.java
+++ src/com/dp/structural/decorator/DecoratorMain.java
@@ -6,6 +6,8 @@ package com.dp.structural.decorator;
  * @author k 2022/8/27 23:17
  */
 public class DecoratorMain {
+
+    // TODO java.io 包中很多用到了装饰器模式
     public static void main(String[] args) {
         Display d1 = new StringDisplay("hello,world!");
         Display d2 = new SideBorder(d1, '#');
