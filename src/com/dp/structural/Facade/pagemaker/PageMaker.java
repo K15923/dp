91744edc75d5490a3eb70c66b9bafd784b476512
wanglinhai <wanglinhai@ejiayou.com>
2022-08-28 23:50:36 +0800

    外观模式

---
+++ src/com/dp/structural/Facade/pagemaker/PageMaker.java
@@ -0,0 +1,25 @@
+package com.dp.structural.Facade.pagemaker;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author k 2022/8/28 23:38
+ */
+public class PageMaker {
+
+    public PageMaker() {
+    }
+
+    public static void makeWelcomePage(String miladdr,String filename) {
+        try {
+            Properties properties = Database.getProperties(filename);
+            String maildata = properties.getProperty(miladdr);
+            HtmlWriter htmlWriter = new HtmlWriter(new FileWriter(filename));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
